ad18a829a5d3883b44bbca985afbcb1d0f1bae0d
masx200 <34191203+masx200@users.noreply.github.com>
2020-07-20 00:48:30 +0000

    Update cli.js

--- cli.js
+++ cli.js
@@ -3,7 +3,7 @@ import {start}from "./index.js"
 const urls = Array.from(new Set(process.argv.slice(2)));
 if (urls.length) {
     console.log(urls);
-    start(urls);
+    start(urls).then(()=>{process.exit(0)});
 } else {
     throw new Error("empty download url,arguments should not be empty");
 }
