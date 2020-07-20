f564a143ca1ddd26f8e08fc87724a059b74179c0
masx200 <34191203+masx200@users.noreply.github.com>
2020-07-20 00:45:52 +0000

    Update cli.js

--- cli.js
+++ cli.js
@@ -1,1 +1,13 @@
+#/bin/env node
 import {start}from "./index.js"
+const urls = Array.from(new Set(process.argv.slice(2)));
+if (urls.length) {
+    console.log(urls);
+    start(urls);
+} else {
+    throw new Error("empty download url,arguments should not be empty");
+}
+import process from "process"
+process.on("unhandledRejection", (e) => {
+    throw e;
+});
