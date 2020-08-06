6750f248e5b5fdeba28465fa8116a71b07683899
masx200 <34191203+masx200@users.noreply.github.com>
2020-08-06 20:28:01 +0800

    Update commonutils.js

--- commonutils.js
+++ commonutils.js
@@ -6,7 +6,8 @@ function getdirectoryname(document) {
     const directoryname =
         //   document.title +
         // "\xA0" +
-        document.querySelectorAll(`.ina > p > b:nth-child(2)`)[0].textContent;
+//有的页面找不到这个TypeError: Cannot read property 'textContent' of undefined
+        document.querySelectorAll(`.ina > p > b:nth-child(2)`)?.[0]?.textContent??document.title;
     return directoryname;
 }
 async function downloadallpagesfromdocument(document) {
