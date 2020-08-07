85cebd904aa811df177e219cd5733f5569c024cc
masx200 <masx200@qq.com>
2020-08-07 21:35:57 +0800

    修复了防盗链下载问题

--- commonutils.js
+++ commonutils.js
@@ -25,30 +25,28 @@ async function downloadallpagesfromdocument(document) {
 }
 async function callaria2cdown(fileurls, directoryname) {
     const data = fileurls.map((url) => {
-const origin=new URL(url).origin
+        const origin = new URL(url).origin + "/";
+        //console.log(origin)
 
-
-return ({
-        jsonrpc: "2.0",
-        method: "aria2.addUri",
-        id: 1,
-        params: [
-            [url],
-            {
-                header: [
-                    "Referer: "+origin,
-                    "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
-                ].join("\n"),
-                dir: directoryname,
-                split: "16",
-                "max-connection-per-server": "16",
-                "seed-ratio": "1.0",
-            },
-        ],
-    })
-
-
-});
+        return {
+            jsonrpc: "2.0",
+            method: "aria2.addUri",
+            id: 1,
+            params: [
+                [url],
+                {
+                    header: [
+                        "Referer: " + origin,
+                        "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
+                    ].join("\n"),
+                    dir: directoryname,
+                    split: "16",
+                    "max-connection-per-server": "16",
+                    "seed-ratio": "1.0",
+                },
+            ],
+        };
+    });
     const response = await fetch("http://localhost:6800/jsonrpc", {
         headers: {
             connection: "keep-alive",
