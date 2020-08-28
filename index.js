5ffc7e5713ebf60864237c31eb87169f5aeb497e
masx200 <34191203+masx200@users.noreply.github.com>
2020-08-28 18:47:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,63 +1,35 @@
-import assert from "assert";
-//import nodefetch from "node-fetch";
-//import xmldom from "xmldom";
+ import assert from "assert";
 import {
     getdirectoryname,
     selectimagesfromdocument,
     downloadallpagesfromdocument,
-    resolvedocumentfromurl,
+    resolvedocumentfromurl
 } from "./commonutils.js";
 import https from "https";
 import http from "http";
 import process from "process";
 http.globalAgent.keepAlive = true;
 https.globalAgent.keepAlive = true;
-//const fetch = nodefetch.default;
-//const { DOMParser } = xmldom;
-//export { DOMParser };
-//export { fetch };
-//import cssselect from "css-select";
-//export { cssselect };
-/*process.on("unhandledRejection", (e) => {
-    throw e;
-});*/
-//const urls = Array.from(new Set(process.argv.slice(2)));*/
-
 const urltodom = new Map();
-
-export { urltodom };
+export {
+    urltodom
+};
 async function start(urls) {
     urls = Array.from(new Set(urls));
     for (let url of urls) {
         url = url.trim();
         checkurl(url);
-
         const document = await resolvedocumentfromurl(url);
-        // const images = selectimagesfromdocument(document);
-        //console.log(images)
-        //  const directoryname = getdirectoryname(document);
-
-        //console.log(url, directoryname, images);
-
         await downloadallpagesfromdocument(document);
-
-        //domtourl.clear();
-
         urltodom.clear();
+        console.log("全部处理完成");
     }
 }
-/*if (urls.length) {
-    console.log(urls);
-    start(urls);
-} else {
-    throw new Error("empty download url,arguments should not be empty");
-}*/
 function checkurl(url) {
     url = new URL(url).href;
-    assert(
-        url.startsWith("http:") || url.startsWith("https:"),
-        "url protocol should be http or https"
-    );
+    assert(url.startsWith("http:") || url.startsWith("https:"), "url protocol should be http or https");
     return url;
 }
-export { start };
+export {
+    start
+};
