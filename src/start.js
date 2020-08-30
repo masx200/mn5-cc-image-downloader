1fdba0f577006a12edd5cec2dd3336653336f4bb
masx200 <masx200@qq.com>
2020-08-30 20:30:27 +0800

    先测试连接rpc成功与否

--- src/start.js
+++ src/start.js
@@ -2,11 +2,13 @@ import { resolvedocumentfromurl } from "./resolvedocumentfromurl.js";
 import { downloadallpagesfromdocument } from "./downloadallpagesfromdocument.js";
 import { checkurl } from "./checkurl.js";
 import { urltodom } from "./index.js";
+import { requestjsonrpc } from "./requestjsonrpc.js";
 
 export async function start(urls) {
     urls = Array.from(new Set(urls)).map((url) => url.trim());
     urls.forEach((url) => checkurl(url));
-    
+    //先测试连接rpc成功与否
+    await requestjsonrpc([]);
     for (let url of urls) {
         // url = url.trim();
         // checkurl(url);
