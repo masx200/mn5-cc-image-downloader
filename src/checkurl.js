ea35924b15963ba002be9cbf073f66118c7fa079
masx200 <masx200@qq.com>
2020-08-30 21:45:30 +0800

    优化

--- src/checkurl.js
+++ src/checkurl.js
@@ -1,10 +1,10 @@
-import assert from "assert";
+//import assert from "assert";
 
 export function checkurl(url) {
     url = new URL(url).href;
-    assert(
+    /* assert(
         url.startsWith("http:") || url.startsWith("https:"),
         "url protocol should be http or https"
-    );
+    );*/
     return url;
 }
