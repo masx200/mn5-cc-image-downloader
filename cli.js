07bf377187a9b491c25871666fb5a28f4bc69f6d
masx200 <34191203+masx200@users.noreply.github.com>
2020-08-30 16:48:49 +0800

    Update cli.js

--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import { start } from "./index.js";
 const urls = Array.from(new Set(process.argv.slice(2))).filter((a) =>
-    a.startsWith("h")
+    a.startsWith("http")
 );
 
 import { helptxt } from "./help.js";
