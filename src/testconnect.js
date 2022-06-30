fb4b8d3361dd73962684973213e26753637bb459
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-30 12:24:42 +0800

    promisify

--- src/testconnect.js
+++ src/testconnect.js
@@ -1,12 +1,20 @@
 import net from "net";
-import { promisify } from "util";
+// import { promisify } from "util";
 import { requestjsonrpc } from "./requestjsonrpc.js";
 import { rpcurl } from "./rpcurl.js";
 
 export async function testconnect() {
     const url = new URL(rpcurl);
     console.log(url);
+    await new Promise((resolve, reject) => {
+        const socket = net.connect(url.port, url.hostname);
+        socket.on("error", reject);
+        socket.on("connect", resolve);
+    });
+
     // @ts-ignore
-    await promisify(net.connect)(Number(url.port), url.hostname);
+    // const s = await promisify(net.connect)(Number(url.port), url.hostname);
+
+    // console.log(s);
     await requestjsonrpc([]);
 }
